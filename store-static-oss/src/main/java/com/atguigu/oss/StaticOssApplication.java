44c5e3c22089cba3696b2dc4a4a8347a7befbfe3
zc123456 <348088053@qq.com>
2022-10-17 16:50:04 +0800

    04_功能实现_静态资源处理和oss服务

---
+++ store-static-oss/src/main/java/com/atguigu/oss/StaticOssApplication.java
@@ -0,0 +1,20 @@
+package com.atguigu.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * projectName: b2c_cloud_store
+ *
+ * @author: 赵伟风
+ * time: 2022/10/17 15:37 周一
+ * description: 启动类
+ */
+@SpringBootApplication
+public class StaticOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticOssApplication.class,args);
+    }
+
+}
